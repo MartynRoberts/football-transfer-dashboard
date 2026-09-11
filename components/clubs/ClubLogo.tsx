ef57bd14fcc710190f0ef7cf3df10bc5a9170ecf
Martyn Roberts <martyn.owen.roberts@gmail.com>
2026-09-11 08:33:53 +0100

    Disabled image optimisation globally due to Vercel restrictions

--- components/clubs/ClubLogo.tsx
+++ components/clubs/ClubLogo.tsx
@@ -33,7 +33,6 @@ export default function ClubLogo({
         sizes={`${size}px`}
         preload={preload}
         fetchPriority={preload ? "high" : "auto"}
-        unoptimized
         onLoad={() => setLoaded(true)}
         onError={() => setFailed(true)}
         className={`size-full object-contain transition-opacity duration-150 ${
